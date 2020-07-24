0b80b2f0d3d4319453b3ae8ccefb12f9b3ee0651
James Muscat <jamesremuscat@gmail.com>
2020-07-24 20:39:41 +0100

    Fixes, and bump version number.

--- src/staldates/VisualsSystem.py
+++ src/staldates/VisualsSystem.py
@@ -50,7 +50,7 @@ class Input(QObject):
     def set_tally(self, tally):
         for me, sources in tally.iteritems():
             self.tally.setdefault(me, {})['pgm'] = self.source in sources['pgm']
-            self.tally.setdefault(me, {})['pvw'] = self.source in sources['pvw']
+            self.tally.setdefault(me, {})['pvw'] = self.source in sources['prv']
         self.changedState.emit()
 
 
